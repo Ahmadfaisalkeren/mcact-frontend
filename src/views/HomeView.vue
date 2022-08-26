e7f623bac16154fe68b5f36773b7a6826a82d589
AhmadFaisalKeren <maulidae.rizki@gmail.com>
2022-08-26 22:05:02 +0700

    vue js frontend

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HeaderMcact />
+    <HeroMcact />
+    <ProductMcact />
+    <InstaMcact />
+    <PartnerMcact />
+    <FooterMcact />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue';
+import HeaderMcact from '@/components/HeaderMcact.vue';
+import HeroMcact from '@/components/HeroMcact.vue';
+import ProductMcact from '@/components/ProductMcact.vue';
+import InstaMcact from '@/components/InstaMcact.vue';
+import PartnerMcact from '@/components/PartnerMcact.vue';
+import FooterMcact from '@/components/FooterMcact.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderMcact,
+    HeroMcact,
+    ProductMcact,
+    InstaMcact,
+    PartnerMcact,
+    FooterMcact,
+  }
+}
+</script>
